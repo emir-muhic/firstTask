bf7e7cb088600fcb2f6a2dc94081029f921786d4
Emir Muhic <emir.muhic1106@gmail.com>
2022-08-27 07:25:43 +0200

    res fix

--- src/Header.js
+++ src/Header.js
@@ -44,12 +44,8 @@ const Header = () => {
                         </div>
                     </div>
                     <div className="px-2 pt-2 pb-3 space-y-1">
-                        <a href="/" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Product</a>
-                        <a href="/" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Features</a>
-                        <a href="/" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Marketplace</a>
-                        <a href="/" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Company</a>
+                        <Nav />
                     </div>
-                    <a href="/" className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"> Log in </a>
                 </div>
             </div>
         </div>
